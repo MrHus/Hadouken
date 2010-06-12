fba6b6e35e0aa21159de6310f79a585b034e42f4
Maarten Hus <MaartenHus@gmail.com>
2010-06-12 12:44:12 +0200

    added some tests

--- test/hadouken/core_test.clj
+++ test/hadouken/core_test.clj
@@ -1,6 +1,11 @@
-(ns clojure-doc.core-test
-  (:use [clojure-doc.core] :reload-all)
+(ns hadouken.core-test
+  (:use [hadouken.core] :reload-all)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false))
+(deftest eval-str-test
+  (is (= 5 (eval-str "(+ 2 3)" *ns*)))
+  (is (= "henk" (eval-str "(str \"henk\")" *ns*)))) 
+
+(deftest parse-template-test
+  (is (= "Hello %s how are you doing, %s thank you." (parse-template "Hello (str \"Maarten\") how are you doing, (+ 10 10) thank you." ["(str \"Maarten\")" "(+ 10 10)"])))
+  (is (= "%s %s %s" (parse-template "(+ 10 10) (- 10 (- 5 3)) (+ 7 (- 7 (+ 4 4)))" ["(+ 10 10)" "(- 10 (- 5 3))" "(+ 7 (- 7 (+ 4 4)))"]))))
